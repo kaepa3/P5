0bbe2513bfb57eeca30725ebb2e678d5002b4dcf
kaepa3 <kaepa3@gmail.com>
2025-12-31 21:07:10 +0900

    P.1.2.1_01 modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,34 @@ import "./style.css";
 import App from "./App.vue";
 import p5 from "p5";
 
-// p という引数（スケッチそのもの）を受け取る関数を作る
-var segmentCount = 360;
-var radius = 150;
-const sketch = (p: p5) => {
+/**
+ * min以上max以下のランダムな整数を取得する
+ */
+const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
+
+const sketch = (p: p5) => {
+  // p という引数（スケッチそのもの）を受け取る関数を作る
+  var colorsLeft: p5.Color[] = [];
+  var colorsRight: p5.Color[] = [];
+  var tileCountX: number;
+  var tileCountY: number;
+  var interCol: p5.Color;
+  var interpolateShortest = true;
+
+  function shakeColors() {
+    for (var i = 0; i < tileCountY; i++) {
+      colorsLeft[i] = p.color(getRandomInt(0, 155), getRandomInt(0, 155), 155);
+      colorsRight[i] = p.color(getRandomInt(0, 155), 55, getRandomInt(0, 55));
+    }
+  }
   p.setup = () => {
-    p.createCanvas(800, 400);
+    p.createCanvas(800, 800);
+    p.colorMode(p.HSB);
     p.noStroke();
-    // colorMode は setup で 1 回だけ設定するのがベスト
-    // H(0-360), S(0-width), B(0-height)
-    p.colorMode(p.HSB, 360, p.width, p.height);
+    shakeColors();
   };
 
   p.draw = () => {
@@ -21,45 +38,44 @@ const sketch = (p: p5) => {
     p.colorMode(p.HSB, 360, p.width, p.height);
     p.background(360, 0, p.height);
 
-    const angleStep = 360 / segmentCount;
+    tileCountX = p.map(p.mouseX, 0, p.width, 2, 100);
+    tileCountY = p.map(p.mouseY, 0, p.height, 2, 10);
+    const tileWidth = p.width / tileCountX;
+    const tileHeight = p.height / tileCountY;
+    const colors = [];
 
     // 2Dモードで色を分けるには、ループ内で三角形を一つずつ描く
-    for (let angle = 0; angle < 360; angle += angleStep) {
-      const vx1 = p.width / 2 + p.cos(p.radians(angle)) * radius;
-      const vy1 = p.height / 2 + p.sin(p.radians(angle)) * radius;
-      const vx2 = p.width / 2 + p.cos(p.radians(angle + angleStep)) * radius;
-      const vy2 = p.height / 2 + p.sin(p.radians(angle + angleStep)) * radius;
-
-      // 1スライスごとに色を指定
-      p.fill(angle, p.mouseX, p.mouseY);
-
-      p.beginShape();
-      p.vertex(p.width / 2, p.height / 2); // 中心
-      p.vertex(vx1, vy1);                 // 現在の角度の点
-      p.vertex(vx2, vy2);                 // 次の角度の点
-      p.endShape(p.CLOSE);
+    for (let gridY = 0; gridY < tileCountY; gridY++) {
+      const col1 = colorsLeft[gridY];
+      const col2 = colorsRight[gridY];
+      for (let gridX = 0; gridX < tileCountX; gridX++) {
+        const amount = p.map(gridX, 0, tileCountX - 1, 0, 1);
+        if (interpolateShortest) {
+          p.colorMode(p.RGB)
+          if (col1 && col2) {
+            interCol = p.lerpColor(col1, col2, amount);
+          }
+          p.colorMode(p.HSB);
+        } else {
+          if (col1 && col2) {
+            interCol = p.lerpColor(col1, col2, amount);
+          }
+        }
+        p.fill(interCol);
+        const posX = tileWidth * gridX;
+        const posY = tileHeight * gridY;
+        p.rect(posX, posY, tileWidth, tileHeight);
+        colors.push(interCol)
+      }
     }
   };
   p.keyPressed = () => {
-    switch (p.key) {
-      case '1':
-        segmentCount = 360;
-        break;
-      case '2':
-        segmentCount = 45;
-        break;
-      case '3':
-        segmentCount = 20;
-        break;
-      case '4':
-        segmentCount = 12;
-        break;
-      case '5':
-        segmentCount = 6;
-        break;
-
-    }
+    if (p.key == '1') interpolateShortest = true;
+    if (p.key == '2') interpolateShortest = false;
   };
+  p.mouseReleased = () => {
+    shakeColors();
+  }
 };
 
 // 実行
