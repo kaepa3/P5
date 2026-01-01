81809517eb0d1fc1e32ceed21633b9507fd1feb1
kaepa3 <kaepa3@gmail.com>
2026-01-01 18:51:19 +0900

    P_2_0_02

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,32 @@ import './lib/generative-design-library.js'; // サイドエフェクト・イ�
 new p5((p: p5) => {
 
   p.setup = () => {
-    p.createCanvas(550, 550);
-    p.strokeCap(p.SQUARE);
+    p.createCanvas(720, 720);
+    p.noFill()
+    p.background(255);
+    p.strokeWeight(2);
+    p.stroke(0, 25);
   };
 
   p.draw = () => {
-    p.background(255);
+    if (p.mouseIsPressed && p.mouseButton.left) {
+      p.push();
+      p.translate(p.width / 2, p.height / 2);
+      var circleResolution = p.map(p.mouseY + 100, 0, p.height, 2, 10);
+      var radius = p.mouseX - p.width / 2;
+      var angle = p.TAU / circleResolution;
+      p.beginShape();
+      for (let i = 0; i < circleResolution; i++) {
+        const x = p.cos(angle * i) * radius;
+        const y = p.sin(angle * i) * radius;
+        p.vertex(x, y);
 
-    p.translate(p.width / 2, p.height / 2);
-
-    var circleResolution = p.map(p.mouseY, 0, p.height, 2, 80);
-    var radius = p.mouseX - p.width / 2 + 0.5;
-    var angle = p.TWO_PI / circleResolution;
-    p.strokeWeight(p.mouseY / 20);
-    p.beginShape();
-    for (let i = 0; i < circleResolution; i++) {
-      const x = p.cos(angle * i) * radius;
-      const y = p.sin(angle * i) * radius;
-      p.line(0, 0, x, y);
+      }
+      p.endShape();
+      p.pop();
     }
-    p.endShape();
   };
+  p.keyReleased = () => {
+    if (p.key == p.DELETE) p.background(255);
+  }
 });
