6c7271282d01576ac2e27eeead4607bcb10c4cb2
kaepa3 <kaepa3@gmail.com>
2026-01-23 22:31:32 +0900

    P_2_1_2_03

--- src/main.ts
+++ src/main.ts
@@ -4,48 +4,32 @@ import './lib/generative-design-library.js'; // サイドエフェクト・イ�
 
 
 new p5((p: p5) => {
-  var tileCount = 20;
-  var actRandomSeed = 0;
+  var moduleColor: p5.Color;
+  var moduleAlpha = 180;
+  var maxDistance = 500;
 
-  var moduleColorBackground: p5.Color;
-  var moduleColorForeground: p5.Color;
-
-  var moduleAlphaBackground = 100;
-  var moduleAlphaForeground = 100;
-
-  var moduleRadiusBackground = 30;
-  var moduleRadiusForeground = 15;
-
-  var backgroundColor: p5.Color;
 
   p.setup = () => {
     p.createCanvas(600, 600);
-    p.colorMode(p.HSB, 360, 100, 100, 100);
-    p.noStroke();
-
-    moduleColorBackground = p.color(0, 0, 0, moduleAlphaBackground);
-    moduleColorForeground = p.color(0, 0, 100, moduleAlphaForeground);
-
-    backgroundColor = p.color(0, 0, 100);
-
+    p.noFill();
+    p.strokeWeight(3);
+    moduleColor = p.color(0, 0, 0, moduleAlpha);
   };
 
   p.draw = () => {
-    p.translate(p.width / tileCount / 2, p.height / tileCount / 2);
-    p.background(backgroundColor);
-    p.randomSeed(actRandomSeed);
-    for (let gridY = 0; gridY < tileCount; gridY++) {
-      for (let gridX = 0; gridX < tileCount; gridX++) {
-        var posX = p.width / tileCount * gridX;
-        var posY = p.height / tileCount * gridY;
-        var shiftX = p.random(-1, 1) * p.mouseX / 20;
-        var shiftY = p.random(-1, 1) * p.mouseY / 20;
+    p.clear();
+    p.stroke(moduleColor);
 
-        p.fill(moduleColorBackground);
-        p.ellipse(posX + shiftX, posY + shiftY, moduleRadiusBackground, moduleRadiusBackground);
+    for (let gridY = 0; gridY < p.width; gridY += 25) {
+      for (let gridX = 0; gridX < p.height; gridX += 25) {
+        var diameter = p.dist(p.mouseX, p.mouseY, gridX, gridY);
+        diameter = diameter / maxDistance * 40;
 
-        p.fill(moduleColorForeground);
-        p.ellipse(posX, posY, moduleRadiusForeground, moduleRadiusForeground);
+        p.push();
+        p.translate(gridX, gridY);
+
+        p.rect(0, 0, diameter, diameter);
+        p.pop();
       }
     }
   };
@@ -55,7 +39,6 @@ new p5((p: p5) => {
   };
 
   p.mousePressed = () => {
-    actRandomSeed = p.random(100000);
   };
 
 
