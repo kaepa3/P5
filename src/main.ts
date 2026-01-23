ac01822244f1853b24be715c97104cad0fe5339a
kaepa3 <kaepa3@gmail.com>
2026-01-23 22:45:17 +0900

    P_2_1_2_04

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,46 @@ import './lib/generative-design-library.js'; // サイドエフェクト・イ�
 
 
 new p5((p: p5) => {
-  var moduleColor: p5.Color;
-  var moduleAlpha = 180;
-  var maxDistance = 500;
+  var tileCount = 20;
+  var actRandomSeed = 0;
+  var rectSize = 30;
 
 
   p.setup = () => {
     p.createCanvas(600, 600);
-    p.noFill();
-    p.strokeWeight(3);
-    moduleColor = p.color(0, 0, 0, moduleAlpha);
+    p.colorMode(p.HSB, 360, 100, 100, 100);
+    p.noStroke();
+    p.fill(192, 100, 64, 60);
   };
 
   p.draw = () => {
     p.clear();
-    p.stroke(moduleColor);
+    p.randomSeed(actRandomSeed);
 
-    for (let gridY = 0; gridY < p.width; gridY += 25) {
-      for (let gridX = 0; gridX < p.height; gridX += 25) {
-        var diameter = p.dist(p.mouseX, p.mouseY, gridX, gridY);
-        diameter = diameter / maxDistance * 40;
+    for (let gridY = 0; gridY < tileCount; gridY++) {
+      for (let gridX = 0; gridX < tileCount; gridX++) {
+        var posX = (p.width / tileCount) * gridX;
+        var posY = (p.height / tileCount) * gridY;
+
+
+        var shiftX1 = p.mouseX / 20 * p.random(-1, 1);
+        var shiftY1 = p.mouseY / 20 * p.random(-1, 1);
+        var shiftX2 = p.mouseX / 20 * p.random(-1, 1);
+        var shiftY2 = p.mouseY / 20 * p.random(-1, 1);
+        var shiftX3 = p.mouseX / 20 * p.random(-1, 1);
+        var shiftY3 = p.mouseY / 20 * p.random(-1, 1);
+        var shiftX4 = p.mouseX / 20 * p.random(-1, 1);
+        var shiftY4 = p.mouseY / 20 * p.random(-1, 1);
 
         p.push();
-        p.translate(gridX, gridY);
+        p.translate(posX, posY);
 
-        p.rect(0, 0, diameter, diameter);
+        p.beginShape();
+        p.vertex(shiftX1, shiftY1);
+        p.vertex(rectSize + shiftX2, shiftY2);
+        p.vertex(rectSize + shiftX3, rectSize + shiftY3);
+        p.vertex(shiftX4, rectSize + shiftY4);
+        p.endShape(p.CLOSE);
         p.pop();
       }
     }
@@ -39,6 +54,7 @@ new p5((p: p5) => {
   };
 
   p.mousePressed = () => {
+    actRandomSeed = p.random(100000);
   };
 
 
