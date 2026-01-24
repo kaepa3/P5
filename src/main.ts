f1cae7a4d7c46ff54cfd6b8ec9bb885c0a68e9e9
kaepa3 <kaepa3@gmail.com>
2026-01-24 17:18:50 +0900

    P_2_1_3_01

--- src/main.ts
+++ src/main.ts
@@ -4,53 +4,70 @@ import './lib/generative-design-library.js'; // サイドエフェクト・イ�
 
 
 new p5((p: p5) => {
-  var tileCount = 20;
-  var actRandomSeed = 0;
-  var rectSize = 30;
+  var tileCountX = 10;
+  var tileCountY = 10;
+  var tileWidth = 0;
+  var tileHeight = 0;
 
+
+  var circleCount = 0;
+  var endSize = 0;
+  var endOffset = 0;
+
+  var actRandomSeed = 0;
 
   p.setup = () => {
-    p.createCanvas(600, 600);
-    p.colorMode(p.HSB, 360, 100, 100, 100);
-    p.noStroke();
-    p.fill(192, 100, 64, 60);
+    p.createCanvas(800, 800);
+    tileWidth = p.width / tileCountX;
+    tileHeight = p.height / tileCountY;
+
+    p.noFill();
+    p.stroke(0, 128);
   };
 
   p.draw = () => {
-    p.clear();
+    p.background(255);
     p.randomSeed(actRandomSeed);
 
-    for (let gridY = 0; gridY < tileCount; gridY++) {
-      for (let gridX = 0; gridX < tileCount; gridX++) {
-        var posX = (p.width / tileCount) * gridX;
-        var posY = (p.height / tileCount) * gridY;
+    p.translate(tileWidth / 2, tileHeight / 2);
 
+    circleCount = p.mouseX / 30 + 1;
+    endSize = p.map(p.mouseX, 0, p.max(p.width, p.mouseX), tileWidth / 2, 0);
+    endOffset = p.map(p.mouseY, 0, p.max(p.height, p.mouseY), 0, (tileWidth - endSize) / 2);
 
-        var shiftX1 = p.mouseX / 20 * p.random(-1, 1);
-        var shiftY1 = p.mouseY / 20 * p.random(-1, 1);
-        var shiftX2 = p.mouseX / 20 * p.random(-1, 1);
-        var shiftY2 = p.mouseY / 20 * p.random(-1, 1);
-        var shiftX3 = p.mouseX / 20 * p.random(-1, 1);
-        var shiftY3 = p.mouseY / 20 * p.random(-1, 1);
-        var shiftX4 = p.mouseX / 20 * p.random(-1, 1);
-        var shiftY4 = p.mouseY / 20 * p.random(-1, 1);
-
+    for (let gridY = 0; gridY < tileCountY; gridY++) {
+      for (let gridX = 0; gridX < tileCountX; gridX++) {
         p.push();
-        p.translate(posX, posY);
+        p.translate(tileWidth * gridX, tileHeight * gridY);
+        p.scale(1, tileHeight / tileWidth);
 
-        p.beginShape();
-        p.vertex(shiftX1, shiftY1);
-        p.vertex(rectSize + shiftX2, shiftY2);
-        p.vertex(rectSize + shiftX3, rectSize + shiftY3);
-        p.vertex(shiftX4, rectSize + shiftY4);
-        p.endShape(p.CLOSE);
+        var toggle = Math.floor(p.random(0, 4));
+        switch (toggle) {
+          case 0:
+            p.rotate(-p.HALF_PI);
+            break;
+          case 1:
+            p.rotate(0);
+            break;
+          case 2:
+            p.rotate(p.HALF_PI);
+            break;
+          case 3:
+            p.rotate(p.PI);
+            break;
+        }
+        for (let i = 0; i < circleCount; i++) {
+          const diameter = p.map(i, 0, circleCount, tileWidth, endSize);
+          var offset = p.map(i, 0, circleCount, 0, endOffset);
+          p.ellipse(offset, 0, diameter, diameter);
+        }
         p.pop();
+
       }
     }
   };
 
   p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 
   p.mousePressed = () => {
